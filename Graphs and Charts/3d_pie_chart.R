cb962bbf541c6a663b95a9f16ddd1f1e79cb5734
Rahul Jain <rahul_jain@students.iitmandi.ac.in>
2018-09-13 18:54:22 +0530

    Added Pie charts

---
+++ Graphs and Charts/3d_pie_chart.R
@@ -0,0 +1,34 @@
+# Syntax
+# pie3D(x, labels, radius, main, col, explode )
+
+# x- vector of data
+
+# Optional paramters
+# labels- description of pies
+# radius- radius of pie (-1<=r<=1)
+# main- title of chart
+# col- color pallete
+# explode- gap between pies
+
+# Import the library.
+library(plotrix)
+
+# Error in library(plotrix) : there is no package called 'plotrix'
+utils:::menuInstallPkgs()
+#Choose Plotrix from the dialog which appears
+
+# Create data for the graph.
+rain <- c(221, 162, 190, 153)
+l <- c("Mandi", "Shimla", "Kullu", "Solan")
+
+# Create a file
+png(file = "3d_pie_chart.jpg")
+
+# Plot the chart
+pie3D(x,labels=l, main="Rainfall in (mm)", explode =0.1, col=rainbow(length(x)))
+
+# To insert a Legend (Optional)
+legend("topright", c("Mandi", "Shimla", "Kullu", "Solan"), cex = 1, fill = rainbow(length(x)))
+
+# Save the file
+dev.off()
---
+++ Graphs and Charts/Pie_chart.R
@@ -0,0 +1,27 @@
+# Syntax
+# pie(x, labels, radius, main, col, clockwise)
+
+# x- vector of data
+
+# Optional paramters
+# labels- description of pies
+# radius- radius of pie (-1<=r<=1)
+# main- title of chart
+# col- color pallete
+# clockwise- logical value indicating order of pies
+
+# Create data for the graph.
+rain <- c(221, 162, 190, 153)
+labels <- c("Mandi", "Shimla", "Kullu", "Solan")
+
+# Create a file
+png(file = "pie_chart.jpg")
+
+# Plot the chart
+pie(x,labels, main="Rainfall in (mm)", clockwise = TRUE, col=c("Blue","red","green","orange"))
+
+# To insert a Legent (Optional)
+legend("topright", c("Mandi", "Shimla", "Kullu", "Solan"), cex = 1, fill = c("Blue","red","green","orange"))
+
+# Save the file
+dev.off()
